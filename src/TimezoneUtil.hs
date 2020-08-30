8348176816a8598d280cf3f71300f4f1da0cdef3
Luis Borjas Reyes <luisfborjas@gmail.com>
2020-08-29 22:44:30 -0400

    Bring in my tiny timezone-detect package for fully offline local -> tz

--- src/TimezoneUtil.hs
+++ src/TimezoneUtil.hs
@@ -5,13 +5,12 @@ import Import
 import RIO.Time
 import Data.Time.LocalTime.TimeZone.Olson
 import Data.Time.LocalTime.TimeZone.Series
-
-type TimeZoneName = FilePath
+import TimezoneDetect
 
 -- | Gets timezone info from the default Unix location for tzdata
 -- we assume /usr/share, but it could also be in /etc:
 -- https://hackage.haskell.org/package/timezone-series-0.1.9/docs/Data-Time-LocalTime-TimeZone-Series.html#g:3
-getTimeZoneSeries :: TimeZoneName -> IO TimeZoneSeries
+getTimeZoneSeries :: TimezoneName -> IO TimeZoneSeries
 getTimeZoneSeries tzName =
     getTimeZoneSeriesFromOlsonFile ("/usr/share/zoneinfo/" <> tzName)
 
@@ -25,13 +24,23 @@ getTimeZoneSeries tzName =
 -- 2019-09-25 06:30:00 UTC
 -- *Main TimezoneUtil> localTimeStringToUTC "America/Tegucigalpa" "2019-12-25 00:30:00"
 -- 2019-12-25 06:30:00 UTC
-localTimeStringToUTC :: TimeZoneName -> String -> IO UTCTime
+localTimeStringToUTC :: TimezoneName -> String -> IO UTCTime
 localTimeStringToUTC tz localTimeStr = do
     series <- getTimeZoneSeries tz
     localTime <- parseTimeM True defaultTimeLocale "%Y-%-m-%-d %T" localTimeStr
     return $ localTimeToUTC' series localTime
 
-toUTC :: TimeZoneName -> LocalTime -> IO UTCTime
+toUTC :: TimezoneName -> LocalTime -> IO UTCTime
 toUTC tz localTime = do
     series <- getTimeZoneSeries tz
     return $ localTimeToUTC' series localTime
+
+-- TODO: wrap in the RIO monad with an `env` that points to the tz db?
+timeAtPointToUTC :: Double -> Double -> LocalTime -> IO (Either String UTCTime)
+timeAtPointToUTC lat lng referenceTime = do
+    let tzName = lookupTimezone "./config/timezone21.bin" (realToFrac lat) (realToFrac lng)
+    case tzName of
+        Left e -> pure $ Left e
+        Right tz -> do
+            t <- toUTC tz referenceTime
+            pure $ Right t
