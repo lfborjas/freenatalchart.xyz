7c03e3699d85acd4f771074dd550130a490def2c
Luis Borjas Reyes <luisfborjas@gmail.com>
2020-08-31 11:04:27 -0400

    First pass at date validation

--- src/Server/Handlers.hs
+++ src/Server/Handlers.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE NoImplicitPrelude, OverloadedStrings #-}
 
 module Server.Handlers where
@@ -8,6 +9,8 @@ import Servant
 import Lucid
 import qualified Views.Index as Index
 import qualified Views.About as About
+import RIO.Time (LocalTime)
+import Validation (Validation(..))
 
 service :: ServerT Service AppM
 service = 
@@ -22,3 +25,27 @@ root = do
 
 about :: AppM (Html ())
 about = return $ About.render
+
+-- HANDLER HELPERS
+
+-- params should be `Required Lenient`
+-- as per: http://hackage.haskell.org/package/servant-0.15/docs/Servant-API-Modifiers.html#t:RequestArgument
+validateDate :: 
+    Either Text Year 
+    -> Either Text Month
+    -> Either Text Day
+    -> Either Text Hour
+    -> Either Text Minute
+    -> Either Text Bool
+    -> Validation (NonEmpty (ChartFormValidationError, Text)) DateParts
+validateDate y m d h mn isAm =
+    DateParts <$> validateDateComponent y 
+              <*> validateDateComponent m
+              <*> validateDateComponent d
+              <*> validateDateComponent h
+              <*> validateDateComponent mn
+              <*> validateDateComponent isAm
+    where
+        invalidDateTimeFailure err = 
+            Failure ( (InvalidDateTime, err) :| [])
+        validateDateComponent = either invalidDateTimeFailure Success
--- src/Server/Types.hs
+++ src/Server/Types.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE DataKinds #-}
@@ -15,3 +16,36 @@ type Service =
     :<|> Raw
 
 type AppM = ReaderT AppContext Servant.Handler
+
+data ChartFormValidationError 
+    = EmptyLocation
+    | InvalidLocation
+    | InvalidDateTime
+    deriving (Eq, Show)
+
+newtype Year = Year Integer 
+    deriving  (Eq, Show, Num)
+
+newtype Month = Month Integer
+    deriving (Eq, Show, Num)
+
+newtype Day = Day Integer
+    deriving (Eq, Show, Num)
+
+newtype Hour = Hour Integer
+    deriving (Eq, Show, Num)
+
+newtype Minute = Minute Integer
+    deriving (Eq, Show, Num)
+
+data DateParts = DateParts 
+    {
+        year :: Year
+    ,   month :: Month
+    ,   day :: Day
+    ,   hour :: Hour
+    ,   minute :: Minute
+    ,   isMorning :: Bool
+    } deriving (Eq, Show)
+
+-- TODO: add instances from query params
