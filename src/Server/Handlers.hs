734466267cfb68c46be66676f8139b3269c10c8a
Luis Borjas Reyes <luisfborjas@gmail.com>
2020-08-31 11:46:56 -0400

    wip: confusion on validation
    * Need to fix the `show` instance for the date part types
    * The two functions being separate means a validation inside
      another validation... there's gotta be a better way,
      maybe I'll need to merge them?

--- src/Server/Handlers.hs
+++ src/Server/Handlers.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE NoImplicitPrelude, OverloadedStrings #-}
 
@@ -9,8 +10,9 @@ import Servant
 import Lucid
 import qualified Views.Index as Index
 import qualified Views.About as About
-import RIO.Time (LocalTime)
+import RIO.Time (defaultTimeLocale, parseTimeM, LocalTime)
 import Validation (Validation(..))
+import RIO.Text (pack)
 
 service :: ServerT Service AppM
 service = 
@@ -30,7 +32,7 @@ about = return $ About.render
 
 -- params should be `Required Lenient`
 -- as per: http://hackage.haskell.org/package/servant-0.15/docs/Servant-API-Modifiers.html#t:RequestArgument
-validateDate :: 
+validateDateParts :: 
     Either Text Year 
     -> Either Text Month
     -> Either Text Day
@@ -38,7 +40,7 @@ validateDate ::
     -> Either Text Minute
     -> Either Text Bool
     -> Validation (NonEmpty (ChartFormValidationError, Text)) DateParts
-validateDate y m d h mn isAm =
+validateDateParts y m d h mn isAm =
     DateParts <$> validateDateComponent y 
               <*> validateDateComponent m
               <*> validateDateComponent d
@@ -49,3 +51,22 @@ validateDate y m d h mn isAm =
         invalidDateTimeFailure err = 
             Failure ( (InvalidDateTime, err) :| [])
         validateDateComponent = either invalidDateTimeFailure Success
+
+validateDate :: 
+    DateParts -> Validation (NonEmpty (ChartFormValidationError, Text)) LocalTime
+validateDate DateParts{..} =
+    maybe 
+        (Failure ((InvalidDateTime, (pack shown) <> " is not a valid date.") :| []))
+        Success
+        parsedTime
+    where
+        parsedTime :: Maybe LocalTime
+        parsedTime = parseTimeM True defaultTimeLocale "%Y-%-m-%-d %l:%-M:%-S %p" shown
+        shown = 
+            (show year) <> "-"
+            <> (show month) <> "-"
+            <> (show day) <> " "
+            <> (show hour) <> ":"
+            <> (show minute) <> ":"
+            <> "00" <> " "
+            <> (if isMorning then "AM" else "PM")
