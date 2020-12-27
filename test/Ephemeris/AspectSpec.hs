56dc9b1184e853b84836cc8cb35e9cebe345c76a
Luis Borjas Reyes <luisfborjas@gmail.com>
2020-12-27 16:01:02 -0500

    Add ability to test other aspects in AspectSpec

--- test/Ephemeris/AspectSpec.hs
+++ test/Ephemeris/AspectSpec.hs
@@ -14,18 +14,20 @@ spec = do
   describe "findAspectAngle" $ do
     let assertions = 
           [
-            ((Longitude 175, Longitude 270), Just (Applying, 5))
-          , ((Longitude 185, Longitude 270), Just (Separating, 5))
-          , ((Longitude 270, Longitude 270), Nothing)
-          , ((Longitude 355, Longitude 270), Just (Applying, 5))
-          , ((Longitude 5,   Longitude 270), Just (Separating, 5))
-          , ((Longitude 270, Longitude 10),  Just (Applying, 10))
-          , ((Longitude 290, Longitude 10),  Just (Separating, 10))
-          , ((Longitude 95,  Longitude 10),  Just (Applying, 5))
-          , ((Longitude 100, Longitude 10),  Just (Exact, 0))
-          , ((Longitude 130, Longitude 10),  Nothing)
+            (sq, (Longitude 175, Longitude 270), Just (Applying, 5))
+          , (sq, (Longitude 185, Longitude 270), Just (Separating, 5))
+          , (sq, (Longitude 270, Longitude 270), Nothing)
+          , (sq, (Longitude 355, Longitude 270), Just (Applying, 5))
+          , (sq, (Longitude 5,   Longitude 270), Just (Separating, 5))
+          , (sq, (Longitude 270, Longitude 10),  Just (Applying, 10))
+          , (sq, (Longitude 290, Longitude 10),  Just (Separating, 10))
+          , (sq, (Longitude 95,  Longitude 10),  Just (Applying, 5))
+          , (sq, (Longitude 100, Longitude 10),  Just (Exact, 0))
+          , (sq, (Longitude 130, Longitude 10),  Nothing)
+          , (cnj, (Longitude 270, Longitude 270), Just (Exact, 0) )
           ]
         sq = Aspect{ aspectType = Major, aspectName = Square, angle = 90.0, maxOrb = 10.0, temperament = Analytical }
-    forM_ assertions $ \((aspecting', aspected'), expected) ->
-      it (printf "Determines square aspect angle and apparent phase between %.2f and %.2f" (aspecting' & getLongitudeRaw) (aspected' & getLongitudeRaw)) $
-        (findAspectAngle sq aspecting' aspected' <&> phaseAndOrb) `shouldBe` expected
+        cnj = Aspect{ aspectType = Major, aspectName = Conjunction, angle = 0.0, maxOrb = 10.0, temperament = Analytical }
+    forM_ assertions $ \(aspect', (aspecting', aspected'), expected) ->
+      it (printf "Determines aspect angle and apparent phase between %.2f and %.2f" (aspecting' & getLongitudeRaw) (aspected' & getLongitudeRaw)) $
+        (findAspectAngle aspect' aspecting' aspected' <&> phaseAndOrb) `shouldBe` expected
