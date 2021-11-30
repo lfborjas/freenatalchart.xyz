6e1051cb9ae51868af97171ee5ee50dba69e894e
Luis Borjas Reyes <luisfborjas@gmail.com>
2021-11-30 17:12:40 -0500

    remove hspec-discover
    it seems to not play nicely with nix-shell, I'm not sure how people deal
    with it: this is all I could find about it:
    
    https://github.com/input-output-hk/haskell.nix/issues/398

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,1 +1,26 @@
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+import Test.Hspec
+
+import qualified ValidationSpec
+import qualified Views.ChartSpec as ChartSpec
+import qualified Views.IndexSpec as IndexSpec
+import qualified Views.TransitsSpec as TransitsSpec
+import qualified Server.HandlersSpec as HandlersSpec
+import qualified Ephemeris.AspectSpec as AspectSpec
+import qualified Ephemeris.HoroscopeSpec as HoroscopeSpec
+import qualified Ephemeris.TransitSpec as TransitSpec
+import qualified Chart.PrerenderedSpec as PrerenderedSpec
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "ValidationSpec" ValidationSpec.spec
+  describe "Views.ChartSpec" ChartSpec.spec
+  describe "Views.IndexSpec" IndexSpec.spec
+  describe "Views.TransitsSpec" TransitsSpec.spec
+  describe "Server.HandlersSpec" HandlersSpec.spec
+  describe "Ephemeris.AspectSpec" AspectSpec.spec
+  describe "Ephemeris.HoroscopeSpec" HoroscopeSpec.spec
+  describe "Ephemeris.TransitSpec" TransitSpec.spec
+  describe "Chart.PrerenderedSpec" PrerenderedSpec.spec
