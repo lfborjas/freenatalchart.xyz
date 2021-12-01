3a0b50f05477c217dedcf51ab33e7b1938c9e045
Luis Borjas Reyes <luisfborjas@gmail.com>
2021-11-30 20:30:03 -0500

    please

--- shell.nix
+++ shell.nix
@@ -11,4 +11,12 @@ in
             nixpkgs.haskellPackages.haskell-language-server
             nixpkgs.tzdata
         ];
+        shellHook = ''
+        if [ ! -d "/usr/share/zoneinfo" ]; then
+            echo "NO ZONEINFO, LINKING";
+            ln -sf ${nixpkgs.tzdata}/share/zoneinfo /usr/share/zoneinfo
+        else
+            echo "ZONEINFO OK";
+        fi
+        '';
     }
