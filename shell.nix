36ca2398ebc87580b97f3116fd1631ee16fb238e
Luis Borjas Reyes <luisfborjas@gmail.com>
2021-11-30 20:17:43 -0500

    include tzdata in nix shell

--- shell.nix
+++ shell.nix
@@ -9,5 +9,6 @@ in
         buildInputs = [
             nixpkgs.haskellPackages.cabal-install
             nixpkgs.haskellPackages.haskell-language-server
+            nixpkgs.tzdata
         ];
     }
